c1d85e822528bf7ac7364a54ae6fc3f98b6b0c27
Aung Bo Naing <chenguoming1@gmail.com>
2024-09-08 11:58:41 +0800

    fixed wrong class name

--- app/Http/Controllers/FiltersController.php
+++ app/Http/Controllers/FiltersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class PricingVariantTemplatesController extends Controller
+class FiltersController extends Controller
 {
     function index(Request $request)
     {
